48039b54245055b810551ff29db8c54a45dfe83b
Dewei Zhai <zeal0502@gmail.com>
2023-09-09 13:06:28 +0200

    Initial commit

---
+++ test/myredshift.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Myredshift from '../lib/myredshift-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/myredshift-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Myredshift.MyredshiftStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
